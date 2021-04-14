e79a93e8ff8f1e241168f7d812b5ce96f06d7769
Pascal <pgerrist@icloud.com>
2021-04-14 18:51:01 +0200

    Modified indexes

--- export.sh
+++ export.sh
@@ -24,7 +24,7 @@ node --max_old_space_size=8000 parseGTFS.js
 echo "[Export]      Inserting in DB"
 mongo liveov --eval 'db.calls.createIndex( { "trip": 1, "date": 1, "stop": 1, "departureTime": 1, "arrivalTime": 1,  } ); db.trips.createIndex( { "trip": 1, "date": 1 } ); db.stops.createIndex( { "stop": 1, "stopArea": 1} ); db.stopareas.createIndex( { "code": 1} );'
 mongoimport --db liveov --collection stopareas --type csv --headerline --file out/stop-areas.csv --upsert --upsertFields code
-mongoimport --db liveov --collection stops --type csv --headerline --file out/stops.csv --upsert --upsertFields stopArea,code
+mongoimport --db liveov --collection stops --type csv --headerline --file out/stops.csv --upsert --upsertFields code
 mongoimport --db liveov --collection trips --type csv --headerline --file out/trips.csv --upsert --upsertFields trip,date
 mongoimport --db liveov --collection calls --type csv --headerline --file out/calls.csv --upsert --upsertFields trip,date
 echo "[Export]      Removing temporary files"
